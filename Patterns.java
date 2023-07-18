a85f3fe49d52df100a50642157689b11623a4354
curateit-dev <save@curateit.com>
2023-07-18 18:59:06 +0530

    butterfly pattern

---
+++ Patterns.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (rows - i); ++j) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+        for (int i = rows; i > 0; i--) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (rows - i); ++j) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
