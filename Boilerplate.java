ba2a264af219dc31a9dcc2b5769ee5a5c715e5c2
curateit-dev <save@curateit.com>
2023-07-18 22:29:09 +0530

    updated arrays

--- Boilerplate.java
+++ Boilerplate.java
@@ -1,18 +1,12 @@
 import java.util.*;
 
 public class Boilerplate {
+    public static void func() {
+
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-            // byte -> short -> int -> float -> long -> double
-            System.out.print("Enter Num : ");
-            int a = sc.nextInt();
-            float converted = a;
-            System.out.println("Converted num is " + converted);
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            sc.close();
-        }
+        int num = 2;
+        func();
     }
 }
