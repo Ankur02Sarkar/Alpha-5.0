606745f1dd46d791839ab13fd3e5c9d0eb999875
curateit-dev <save@curateit.com>
2023-07-18 19:43:00 +0530

    diamond

--- Patterns.java
+++ Patterns.java
@@ -7,19 +7,24 @@ public class Patterns {
         System.out.println("Enter the number of rows: ");
         int rows = scanner.nextInt();
         for (int i = 1; i <= rows; ++i) {
-            for (int j = rows - i + 1; j > 0; --j) {
+            for (int j = rows - i; j > 0; --j) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= rows; j++) {
-                if (j == 1 || j == rows || i == 1 || i == rows) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
 
+        for (int i = rows - 1; i >= 1; --i) {
+            for (int j = 0; j < rows - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         scanner.close();
     }
 }
